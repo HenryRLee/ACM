27f32da58c53d569985930b63bef2e7d855408af
HenryLee <lee0906@hotmail.com>
2015-07-06 17:44:59 +1000

    Add template

---
+++ template.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+#define fl(i, b, e)		for (int i=(int)(b); i<(int)(e); i++)
+#define fi(b, e)	fl(i, b, e)
+#define fio(e)	fi(0, e)
+#define fj(b, e)	fl(j, b, e)
+#define fjo(e)	fj(0, e)
+#define pb	push_back
+#define sz(x)	((int)(x).size())
+#define mp	make_pair
+#define fs	first
+#define sd	second
+#define all(x)	(x).begin(), (x).end()
+#define sqr(x)	((x)*(x))
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <ll> vl;
+typedef vector <vi> vvi;
+typedef vector <vl> vvl;
+typedef pair <int, int> pii;
+
+inline int getint()
+{
+	int a;
+	cin >> a;
+	return a;
+}
+
+inline ll getll()
+{
+	ll a;
+	cin >> a;
+	return a;
+}
+
+inline double getdouble()
+{
+	double a;
+	cin >> a;
+	return a;
+}
+
+int main(void)
+{
+
+	return 0;
+}
