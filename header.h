546dd106f4426763fa8581481018b5bd45eb7f61
HenryLee <lee0906@hotmail.com>
2015-07-06 21:35:58 +1000

    Add header

---
+++ header.h
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <set>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+#define fl(i, b, e)		for (int i=(int)(b); i<(int)(e); i++)
+#define flo(i, e)		fl(i, 0, e)
+#define fi(b, e)	fl(i, b, e)
+#define fio(e)	fi(0, e)
+#define fj(b, e)	fl(j, b, e)
+#define fjo(e)	fj(0, e)
+#define fk(b, e)	fl(k, b, e)
+#define fko(e)	fk(0, e)
+#define pb	push_back
+#define sz	size()
+#define mp	make_pair
+#define fs	first
+#define sd	second
+#define all(x)	(x).begin(), (x).end()
+#define sqr(x)	((x)*(x))
+
+typedef long long ll;
+typedef vector <int> vi;
+typedef vector <ll> vl;
+typedef vector <vi> vvi;
+typedef vector <vl> vvl;
+typedef pair <int, int> pii;
+
+inline bool cbound(int x, int b, int e)
+{
+	return ((x >= b) && (x <= e));
+}
+
+inline int getint()
+{
+	int a;
+	cin >> a;
+	return a;
+}
+
+inline ll getll()
+{
+	ll a;
+	cin >> a;
+	return a;
+}
+
+inline double getdouble()
+{
+	double a;
+	cin >> a;
+	return a;
+}
+
