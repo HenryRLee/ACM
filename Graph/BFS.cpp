3b54e78b5bcdb933b08065757a5c55f9c0bfb4cd
HenryLee <lee0906@hotmail.com>
2015-07-08 14:37:45 +1000

    Add BFS on matrix

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -46,3 +46,51 @@ int BFSList(vvi list, int src, int dst)
 
 	return 0;
 }
+
+int BFSMatrix(vvi matrix, int src, int dst)
+{
+	vi seen;
+	vi dist;
+	queue <int> q;
+
+	seen.resize(matrix.sz);
+	dist.resize(matrix.sz);
+
+	seen[src] = 1;
+	dist[src] = 0;
+
+	q.push(src);
+
+	while (!q.empty())
+	{
+		int u;
+		u = q.front();
+		q.pop();
+
+		flo(v, matrix[u].sz)
+		{
+			if (matrix[u][v] == 1)
+			{
+				if (seen[v] == 0)
+				{
+					/* First seen */
+					seen[v] = 1;
+					dist[v] = dist[u] + 1;
+
+					if (v == dst)
+					{
+						/* Found */
+						return dist[v];
+					}
+					else
+					{
+						/* Not found */
+						q.push(v);
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
