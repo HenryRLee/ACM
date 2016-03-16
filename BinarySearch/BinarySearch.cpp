ff57286741758e9a85154416c52aa9a27230a6e3
HenryLee <lee0906@hotmail.com>
2016-03-16 15:50:31 +1100

    Modify BinarySearch

--- BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch.cpp
@@ -3,12 +3,23 @@
 /*
  * Checking function for binary search
  */
-bool check(ll x)
+bool check(ll x, const vi &array)
 {
-	return (x <= 100);
+	/*
+	 * If the element you want is on the right side, you can still use
+	 * this comparison, but remember to change the less/less or equal sign.
+	 * In the end, increment the result by 1.
+	 *
+	 * For example, you want the number strictly greater than 100,
+	 * use array[x] <= 100, and increment the final result by 1.
+	 */
+	if (array[x] <= 100)
+		return true;
+	else
+		return false;
 }
 
-ll bsearch(ll minx, ll maxx)
+ll bsearch(ll minx, ll maxx, const vi &array)
 {
 	ll l = minx;
 	ll r = maxx;
@@ -17,13 +28,13 @@ ll bsearch(ll minx, ll maxx)
 	{
 		ll mid = (l + r) / 2;
 
-		if (check(mid))
+		if (check(mid, array))
 			l = mid + 1;
 		else
 			r = mid - 1;
 	}
 
-	if (check(l))
+	if (check(l, array))
 		return l;
 	else
 		return (l - 1);
@@ -31,5 +42,8 @@ ll bsearch(ll minx, ll maxx)
 
 int main(void)
 {
-	cout << bsearch(0, 1e18) << endl;
+	int a[] = {0, 2, 4, 8, 16, 32, 64, 100, 128, 256, 512};
+	vi array(a, a + sizeof(a)/sizeof(int));
+
+	cout << bsearch(0, sizeof(a)/sizeof(int), array) << endl;
 }
